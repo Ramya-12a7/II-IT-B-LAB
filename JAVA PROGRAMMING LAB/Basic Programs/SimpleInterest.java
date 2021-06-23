32e2975256c2c8190d7dc9c7b6d1051b0be65775
Ramya-12a7 <85625747+Ramya-12a7@users.noreply.github.com>
2021-06-23 11:02:10 +0530

    Add files via upload

---
+++ JAVA PROGRAMMING LAB/Basic Programs/SimpleInterest.java
@@ -0,0 +1,12 @@
+class SimpleInterest
+{
+public static void main(String[] args)
+{
+int p=10,t=19,r=10;
+float SimpleInterest;
+
+SimpleInterest=p*t*r/100;
+
+System.out.println(SimpleInterest);
+}
+}
